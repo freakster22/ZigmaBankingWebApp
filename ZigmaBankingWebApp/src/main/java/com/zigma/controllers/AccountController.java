897fe41a080f102ae3da4df803b2b5955a4080e9
Arib Anwar <55917218+freakster22@users.noreply.github.com>
2019-12-04 08:09:01 +0530

    Added Missing annotations

--- ZigmaBankingWebApp/src/main/java/com/zigma/controllers/AccountController.java
+++ ZigmaBankingWebApp/src/main/java/com/zigma/controllers/AccountController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.spring.framework.seb.bind.annotation.PathVariable;
 
 import com.zigma.entities.Account;
 import com.zigma.services.AccountService;
@@ -18,7 +19,7 @@ public class AccountController {
 
 
 	@GetMapping("/cust/{id}")
-	public Account findAccountByAccountNo(long accountNo) {
+	public Account findAccountByAccountNo(@PathVariable("id") long accountNo) {
 		return accountService.findAccountByAccountNo(accountNo);
 	}
 
